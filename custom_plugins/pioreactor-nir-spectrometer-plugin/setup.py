1eb03134d1ef9d02934a97e072b4031e0de036ed
Tobieausb <62897442+Tobieausb@users.noreply.github.com>
2026-09-11 12:34:51 +0200

    Bump optical sweep plugin to v0.2.0

--- custom_plugins/pioreactor-nir-spectrometer-plugin/setup.py
+++ custom_plugins/pioreactor-nir-spectrometer-plugin/setup.py
@@ -7,21 +7,17 @@ from setuptools import setup
 
 setup(
     name="pioreactor-nir-spectrometer-plugin",
-    version="0.1.0",
+    version="0.2.0",
     license="MIT",
     license_files=("LICENSE.txt",),
-    description="NIR transmission and apparent-OD sweeps using an Adafruit AS7341 and an external Pioreactor LED channel.",
+    description="Paired AS7341 NIR transmission and blue-excited 515 nm optical sweeps for Pioreactor.",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     author="Tobias Ebbing",
     url="https://github.com/Tobieausb/pioreactor",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        "spectrometer-reading-plugin>=0.4.0",
-    ],
-    entry_points={
-        "pioreactor.plugins": "nir_spectrometer_plugin = nir_spectrometer_plugin"
-    },
+    install_requires=["spectrometer-reading-plugin>=0.4.0"],
+    entry_points={"pioreactor.plugins": "nir_spectrometer_plugin = nir_spectrometer_plugin"},
     python_requires=">=3.11",
 )
